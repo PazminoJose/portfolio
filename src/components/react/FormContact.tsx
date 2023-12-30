4cbe409b6bf2bde16b1f62bd6c33de7994d141f3
José Pazmiño <pepe.paz0399@gmail.com>
2023-12-29 21:57:22 -0500

    fix: change language in input labels

--- src/components/react/FormContact.tsx
+++ src/components/react/FormContact.tsx
@@ -4,9 +4,9 @@ import { Input, Textarea } from "@nextui-org/input";
 export default function FormContact() {
   return (
     <form className="flex w-full flex-col gap-5  sm:w-[40rem]">
-      <Input label="Nombre" className="" />
-      <Input label="Correo" type="email" className="" />
-      <Textarea label="Mensaje" className="" />
+      <Input label="Name" className="" />
+      <Input label="Email" type="email" className="" />
+      <Textarea label="Message" className="" />
       <Button variant="faded" className="bg-slate-950/50 text-lg font-semibold uppercase">
         Send
       </Button>
