6fc38aa96860dff16609eae335afabb47e5215fd
José Pazmiño <pepe.paz0399@gmail.com>
2024-12-20 17:39:41 -0500

    fix: reorder imports in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/serverless";
-
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
+import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
