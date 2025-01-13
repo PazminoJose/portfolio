93956c6855a6c058e5a02e4102f0219476932ebb
José Pazmiño <pepe.paz0399@gmail.com>
2025-01-13 18:28:12 -0500

    feat: update bachelor degree

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -5,11 +5,23 @@ export const education: TimeLineElement[] = [
   {
     icon: Books,
     date: "2016",
-    title: "Bachelor's degree",
-    description: "Science Bachelor",
+    title: "BACHELOR´S DEGREE IN SOFTWARE ENGINEERING",
+    description: "UNIVERSIDAD TÉCNICA DE AMBATO",
     links: [
       {
         title: "View Bachelor's Degree",
+        href: "/docs/titulo.pdf"
+      }
+    ]
+  },
+  {
+    icon: Books,
+    date: "2016",
+    title: "HIGH SCHOOL SCIENCE DEGREE",
+    description: "JUAN LEÓN MERA LA SALLE HIGH SCHOOL",
+    links: [
+      {
+        title: "View High School Segree",
         href: "/docs/titulo%20bachiller.pdf"
       }
     ]
